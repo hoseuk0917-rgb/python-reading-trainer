ed86841e10d6e056d34e843a0b4382740ff905be
Hoseuk <hoseuk0917@example.com>
2026-06-01 21:38:09 +0900

    Add function scope reading note cards

--- src/pwa/app.js
+++ src/pwa/app.js
@@ -1,5 +1,5 @@
 // === CACHE BUST START ===
-const APP_DATA_VERSION = "20260601_v96_a2";
+const APP_DATA_VERSION = "20260601_v96_a3";
 function withDataVersion(path) {
   if (typeof path !== "string") return path;
   if (path.indexOf("?") >= 0) return path + "&v=" + APP_DATA_VERSION;
@@ -1024,7 +1024,8 @@ async function init() {
     "../../data/lessons/python_foundation_level3_v95_a4_file_exception_path.json",
     "../../data/lessons/python_foundation_level4_v95_a5_oop_basics.json",
     "../../data/lessons/python_beginner_mixed_review_v96_a1.json",
-    "../../data/lessons/python_beginner_reading_notes_v96_a2.json"
+    "../../data/lessons/python_beginner_reading_notes_v96_a2.json",
+    "../../data/lessons/python_function_scope_reading_notes_v96_a3.json"
   ];
 
   const lessonResults = await Promise.all(lessonFiles.map(function(path) {
@@ -1055,7 +1056,8 @@ async function init() {
     "../../data/side_cards/python_foundation_level3_side_cards_v95_a4_file_exception_path.json",
     "../../data/side_cards/python_foundation_level4_side_cards_v95_a5_oop_basics.json",
     "../../data/side_cards/python_beginner_mixed_review_side_cards_v96_a1.json",
-    "../../data/side_cards/python_beginner_reading_notes_side_cards_v96_a2.json"
+    "../../data/side_cards/python_beginner_reading_notes_side_cards_v96_a2.json",
+    "../../data/side_cards/python_function_scope_reading_notes_side_cards_v96_a3.json"
   ];
 
   const sideResults = await Promise.all(sideFiles.map(function(path) {
