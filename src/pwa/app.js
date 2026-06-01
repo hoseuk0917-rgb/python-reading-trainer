795cabb32d0b275a5bc003cb41b07c2908480258
Hoseuk <hoseuk0917@example.com>
2026-06-01 20:52:12 +0900

    Add level 3 dict tuple set foundation cards

--- src/pwa/app.js
+++ src/pwa/app.js
@@ -1,5 +1,5 @@
 // === CACHE BUST START ===
-const APP_DATA_VERSION = "20260531_v95_a1";
+const APP_DATA_VERSION = "20260531_v95_a2";
 function withDataVersion(path) {
   if (typeof path !== "string") return path;
   if (path.indexOf("?") >= 0) return path + "&v=" + APP_DATA_VERSION;
@@ -1018,7 +1018,8 @@ async function init() {
     "../../data/lessons/python_foundation_beginner_v94_a1_part2.json",
     "../../data/lessons/python_foundation_level2_v94_a2_part1.json",
     "../../data/lessons/python_foundation_level2_v94_a2_part2.json",
-    "../../data/lessons/python_foundation_level3_v95_a1_functions.json"
+    "../../data/lessons/python_foundation_level3_v95_a1_functions.json",
+    "../../data/lessons/python_foundation_level3_v95_a2_dict_tuple_set.json"
   ];
 
   const lessonResults = await Promise.all(lessonFiles.map(function(path) {
@@ -1043,7 +1044,8 @@ async function init() {
     "../../data/side_cards/python_foundation_side_cards_v94_a1_part2.json",
     "../../data/side_cards/python_foundation_level2_side_cards_v94_a2_part1.json",
     "../../data/side_cards/python_foundation_level2_side_cards_v94_a2_part2.json",
-    "../../data/side_cards/python_foundation_level3_side_cards_v95_a1_functions.json"
+    "../../data/side_cards/python_foundation_level3_side_cards_v95_a1_functions.json",
+    "../../data/side_cards/python_foundation_level3_side_cards_v95_a2_dict_tuple_set.json"
   ];
 
   const sideResults = await Promise.all(sideFiles.map(function(path) {
