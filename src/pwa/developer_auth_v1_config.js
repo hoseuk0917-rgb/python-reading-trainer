b7dc97dbe131d1b20f0beea57d40dc71adf2e46f
hoseuk0917-rgb <hoseuk0917@gmail.com>
2026-08-21 04:41:39 +0900

    V400.1: load release polish overlay

--- src/pwa/developer_auth_v1_config.js
+++ src/pwa/developer_auth_v1_config.js
@@ -7,4 +7,12 @@
         ownerGithubId: "238496232"
     });
 
+    if (!document.querySelector('script[data-v400-release-polish]')) {
+        const script = document.createElement("script");
+        script.src = "./v400_release_polish.js?v=20260821_v400_1_release_polish1";
+        script.defer = true;
+        script.setAttribute("data-v400-release-polish", "1");
+        document.head.appendChild(script);
+    }
+
 })();
