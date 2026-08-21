1274ef317211eb1712268ba5f5f62be6f51015b5
hoseuk0917-rgb <hoseuk0917@gmail.com>
2026-08-21 10:22:12 +0900

    V400.5: make runtime preloader network-first

--- src/pwa/sw_v400_1.js
+++ src/pwa/sw_v400_1.js
@@ -1,6 +1,6 @@
 const CACHE_PREFIX = "python-reading-trainer-v400";
-const RELEASE = "20260821_v400_5";
-const CACHE_NAME = "python-reading-trainer-v400-5-20260821";
+const RELEASE = "20260821_v400_5_runtime1";
+const CACHE_NAME = "python-reading-trainer-v400-5-runtime1-20260821";
 
 const SHELL_ASSETS = [
   "./",
@@ -10,7 +10,7 @@ const SHELL_ASSETS = [
   "./consumer_ui_v349.css",
   "./release_polish_v400_1.css",
   "./app.js",
-  "./runtime_lesson_bundle_v400_5.js",
+  "./content_quality_semantics.js",
   "./learning_home_v343.js",
   "./consumer_ux_v349.js",
   "./release_polish_v400_1.js",
@@ -26,7 +26,7 @@ const CRITICAL_UI_FILES = new Set([
   "release_polish_v400_1.css",
   "release_polish_v400_1.js",
   "v400_release_polish.js",
-  "runtime_lesson_bundle_v400_5.js",
+  "content_quality_semantics.js",
   "developer_auth_v1_config.js"
 ]);
 
