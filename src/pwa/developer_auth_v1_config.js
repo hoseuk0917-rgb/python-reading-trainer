bd4ed6c233b406fec26cc9145d37daa75123b6fa
hoseuk0917-rgb <hoseuk0917@gmail.com>
2026-08-21 16:07:33 +0900

    V400.6.1: load Developer entry in More menu

--- src/pwa/developer_auth_v1_config.js
+++ src/pwa/developer_auth_v1_config.js
@@ -2,7 +2,7 @@
     "use strict";
 
     window.PRTDeveloperAuthV1Config = Object.freeze({
-        version: "V400.6.1_DEVELOPER_AUTH_GITHUB_OAUTH1",
+        version: "V400.6.1_DEVELOPER_AUTH_GITHUB_OAUTH_ENTRY1",
         authBase: "https://veriautonomy.com/api/prt-developer",
         ownerGithubId: "238496232"
     });
@@ -20,6 +20,14 @@
         script.src = "./v400_release_polish.js?v=20260821_v400_3_release_polish";
         script.async = false;
         script.setAttribute("data-v400-release-polish-v3", "1");
+        document.head.appendChild(script);
+    }
+
+    if (!document.querySelector('script[data-v400-developer-remote-entry]')) {
+        const script = document.createElement("script");
+        script.src = "./developer_remote_entry_v400_6_1.js?v=20260821_v400_6_1_dev_entry1";
+        script.async = false;
+        script.setAttribute("data-v400-developer-remote-entry", "1");
         document.head.appendChild(script);
     }
 })();
